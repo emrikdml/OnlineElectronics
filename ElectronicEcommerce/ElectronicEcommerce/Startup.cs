653e8aebaddec1ec1ac3825a0ae2f3ae0fdb6e39
emrikdml <emrikdml@gmail.com>
2018-05-11 17:57:08 -0500

    Project Created

---
+++ ElectronicEcommerce/ElectronicEcommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElectronicEcommerce.Startup))]
+namespace ElectronicEcommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
